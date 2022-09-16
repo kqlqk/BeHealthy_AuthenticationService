27495e5222ec0f756a53393cf022ed0c53b00b43
kqlqk <kononchuk99999@gmail.com>
2022-09-16 22:03:05 +0300

    init

---
+++ src/main/java/me/kqlqk/behealthy/authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,21 @@
+package me.kqlqk.behealthy.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class AuthenticationServiceApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AuthenticationServiceApplication.class);
+    }
+
+}
