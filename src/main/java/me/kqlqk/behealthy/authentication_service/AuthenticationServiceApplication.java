12b296800486d53e87e0538f3f3ad5d58b2493a4
kqlqk <kononchuk99999@gmail.com>
2022-10-18 22:18:53 +0200

    bugfix :)

--- src/main/java/me/kqlqk/behealthy/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/me/kqlqk/behealthy/authentication_service/AuthenticationServiceApplication.java
@@ -3,8 +3,10 @@ package me.kqlqk.behealthy.authentication_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthenticationServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
