eb70bca56920706350c80bdfa4bb97d94a605017
kqlqk <kononchuk99999@gmail.com>
2022-10-18 22:05:12 +0200

    prepare before deploy on tomcat

--- src/main/java/me/kqlqk/behealthy/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/me/kqlqk/behealthy/authentication_service/AuthenticationServiceApplication.java
@@ -2,7 +2,6 @@ package me.kqlqk.behealthy.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,11 +9,6 @@ public class AuthenticationServiceApplication extends SpringBootServletInitializ
 
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServiceApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AuthenticationServiceApplication.class);
     }
 
 }
