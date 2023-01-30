701d19edf1f3484dcf59d58ca4234e3ab736304d
kqlqk <kononchuk99999@gmail.com>
2023-01-30 21:31:22 +0100

    update TokensDTO
    * was added TokensDTO#userId

--- src/main/java/me/kqlqk/behealthy/authentication_service/dto/TokensDTO.java
+++ src/main/java/me/kqlqk/behealthy/authentication_service/dto/TokensDTO.java
@@ -2,12 +2,20 @@ package me.kqlqk.behealthy.authentication_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TokensDTO {
     private final String type = "Bearer";
 
+    private long userId;
     private String accessToken;
     private String refreshToken;
+
+    public TokensDTO(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
